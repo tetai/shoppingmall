7d24fa3f389f85d37ec68b45831cd4abef4bdd98
zhengkanzhong <zhengkanzhong@meizu.com>
2021-08-02 18:17:43 +0800

    添加Redis

---
+++ shopping-admin-api/src/main/java/org/linlinjava/litemall/admin/web/RedisController.java
@@ -0,0 +1,35 @@
+package org.linlinjava.litemall.admin.web;
+
+import org.linlinjava.litemall.db.redis.RedisUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RequestMapping("/redis")
+@RestController
+public class RedisController {
+
+    private static int ExpireTime = 60;   // redis中存储的过期时间60s
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @RequestMapping("set")
+    public boolean redisset(String key, String value){
+
+        //return redisUtil.set(key,userEntity,ExpireTime);
+
+        return redisUtil.set(key,value);
+    }
+
+    @RequestMapping("get")
+    public Object redisget(String key){
+        return redisUtil.get(key);
+    }
+
+    @RequestMapping("expire")
+    public boolean expire(String key){
+        return redisUtil.expire(key,ExpireTime);
+    }
+}
